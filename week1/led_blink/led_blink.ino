c251a3ba7eb6a5bf969b714fe5bdcc74fdb0fa75
priyanshusinghbohra22-pixel <priyanshusinghbohra22@gmail.com>
2026-07-05 01:19:21 +0530

    Added LED Blink code
    Implemented LED blinking program with a 1-second ON/OFF delay.

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,1 +1,20 @@
+/*
+Author: Priyanshu Singh Bohra
+Date: 05-07-2026
+Description: LED Blink Program
 
+The LED connected to pin 13 turns ON for one second
+and OFF for one second repeatedly.
+*/
+
+void setup() {
+    pinMode(13, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(13, HIGH);
+    delay(1000);
+
+    digitalWrite(13, LOW);
+    delay(1000);
+}
