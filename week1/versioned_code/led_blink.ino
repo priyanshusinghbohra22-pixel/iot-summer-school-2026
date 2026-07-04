021dd138e66e12e85b3013595b09665b9f5f1c13
priyanshusinghbohra22-pixel <priyanshusinghbohra22@gmail.com>
2026-07-05 03:04:05 +0530

    feat: add serial print for blink counter

--- week1/versioned_code/led_blink.ino
+++ week1/versioned_code/led_blink.ino
@@ -1,11 +1,21 @@
+int count = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
+
   digitalWrite(13, HIGH);
   delay(500);
 
   digitalWrite(13, LOW);
   delay(500);
+
+  count++;
+
+  Serial.print("Blink Count: ");
+  Serial.println(count);
+
 }
