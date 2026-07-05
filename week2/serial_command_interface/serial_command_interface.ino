45c41e37eb5d757816b9099614e254c80f667469
priyanshusinghbohra22-pixel <priyanshusinghbohra22@gmail.com>
2026-07-05 13:26:07 +0530

    added LED ON and LED OFF command over serial monitor

--- week2/serial_command_interface/serial_command_interface.ino
+++ week2/serial_command_interface/serial_command_interface.ino
@@ -1,1 +1,48 @@
+/*
+Author: Priyanshu Singh Bohra
+Commit 1
 
+Features:
+LED_ON
+LED_OFF
+*/
+
+const int led = 2;
+
+void setup() {
+
+  pinMode(led, OUTPUT);
+
+  Serial.begin(115200);
+
+  Serial.println("Serial Command Interface Ready");
+
+}
+
+void loop() {
+
+  if (Serial.available()) {
+
+    String cmd = Serial.readStringUntil('\n');
+
+    cmd.trim();
+
+    if (cmd == "LED_ON") {
+
+      digitalWrite(led, HIGH);
+
+      Serial.println("LED ON");
+
+    }
+
+    else if (cmd == "LED_OFF") {
+
+      digitalWrite(led, LOW);
+
+      Serial.println("LED OFF");
+
+    }
+
+  }
+
+}
